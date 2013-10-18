5d4c37ade94d576c494949a814a3aad4ee3e8b1f
Rodrigo <rodrigoevaldes@hotmail.com>
2013-10-18 01:41:14 -0300

    Se agrega el test UtilsJunit3Test.java

---
+++ Junit-Sample/test/sample/UtilsJunit3Test.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Rodrigo Valdés <your.name at your.org>
+ */
+public class UtilsJunit3Test extends TestCase {
+    
+    public UtilsJunit3Test(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        System.out.println("* UtilsJUnit3Test: setUp() method");
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        System.out.println("* UtilsJUnit3Test: tearDown() method");
+    }
+
+    /**
+     * Test of concatWords method, of class Utils.
+     */
+    /* public void testConcatWords() {
+     * System.out.println("concatWords");
+     * String[] words = null;
+     * String expResult = "";
+     * String result = Utils.concatWords(words);
+     * assertEquals(expResult, result);
+     * // TODO review the generated test code and remove the default call to fail.
+     * fail("The test case is a prototype.");
+     * }*/
+
+    /**
+     * Test of computeFactorial method, of class Utils.
+     */
+    public void testComputeFactorial() {
+        System.out.println("computeFactorial");
+        int number = 0;
+        String expResult = "";
+        String result = Utils.computeFactorial(number);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of normalizeWord method, of class Utils.
+     */
+    public void testNormalizeWord() {
+        System.out.println("normalizeWord");
+        String word = "";
+        String expResult = "";
+        String result = Utils.normalizeWord(word);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
