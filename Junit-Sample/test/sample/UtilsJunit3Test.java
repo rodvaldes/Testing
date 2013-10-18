da47d17ecbe01dee7ad47f9d7420df52e432897b
Rodrigo <rodrigoevaldes@hotmail.com>
2013-10-18 02:08:56 -0300

    Se actualiza los test de Utils

--- Junit-Sample/test/sample/UtilsJunit3Test.java
+++ Junit-Sample/test/sample/UtilsJunit3Test.java
@@ -4,6 +4,7 @@
  */
 package sample;
 
+import java.util.concurrent.TimeoutException;
 import junit.framework.TestCase;
 
 /**
@@ -51,26 +52,61 @@ public class UtilsJunit3Test extends TestCase {
     /**
      * Test of computeFactorial method, of class Utils.
      */
-    public void testComputeFactorial() {
-        System.out.println("computeFactorial");
-        int number = 0;
-        String expResult = "";
-        String result = Utils.computeFactorial(number);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
+    /* public void testComputeFactorial() {
+     * System.out.println("computeFactorial");
+     * int number = 0;
+     * String expResult = "";
+     * String result = Utils.computeFactorial(number);
+     * assertEquals(expResult, result);
+     * // TODO review the generated test code and remove the default call to fail.
+     * fail("The test case is a prototype.");
+     * }*/
     /**
      * Test of normalizeWord method, of class Utils.
      */
-    public void testNormalizeWord() {
-        System.out.println("normalizeWord");
-        String word = "";
-        String expResult = "";
-        String result = Utils.normalizeWord(word);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    /*public void testNormalizeWord() {
+     * System.out.println("normalizeWord");
+     * String word = "";
+     * String expResult = "";
+     * String result = Utils.normalizeWord(word);
+     * assertEquals(expResult, result);
+     * // TODO review the generated test code and remove the default call to fail.
+     * fail("The test case is a prototype.");
+     * }*/
+    
+    public void DISABLED_testTemporarilyDisabled() throws Exception {
+        System.out.println("* UtilsJUnit3Test: test method 4 - checkExpectedException()");
+        assertEquals("Malm\u00f6", Utils.normalizeWord("Malmo\u0308"));
     }
+    
+    public void testWithTimeout() throws InterruptedException, TimeoutException {
+        System.out.println("* UtilsJUnit3Test: test method 2 - testWithTimeout()");
+        final int factorialOf = 1 + (int) (30000 * Math.random());
+        System.out.println("computing " + factorialOf + '!');
+
+        Thread testThread = new Thread() {
+            public void run() {
+                System.out.println(factorialOf + "! = " + Utils.computeFactorial(factorialOf));
+            }
+        };
+        testThread.start();
+        Thread.sleep(1000);
+        testThread.interrupt();
+
+        if (testThread.isInterrupted()) {
+            throw new TimeoutException("the test took too long to complete");
+        }
+    }
+    
+    public void testExpectedException() {
+        System.out.println("* UtilsJUnit3Test: test method 3 - testExpectedException()");
+        try {
+            final int factorialOf = -5;
+            System.out.println(factorialOf + "! = " + Utils.computeFactorial(factorialOf));
+            fail("IllegalArgumentException was expected");
+        } catch (IllegalArgumentException ex) {
+        }
+    }
+    
+    
 }
