75996be4ea61ca55b0d7ddb9a0876f1251fc3a17
Rodrigo <rodrigoevaldes@hotmail.com>
2014-05-27 15:13:32 -0400

    Se agregan al tracking el provisioning.

---
+++ VagrantEnvironments/LampEnv/provision/manifests/default.pp
@@ -0,0 +1,10 @@
+import "apache"
+include apache
+import "modrewrite"
+include modrewrite
+import "php"
+include php
+import "mysql"
+include mysql
+import "mail"
+include mail
---
+++ VagrantEnvironments/LampEnv/provision/modules/postfix/init.pp
@@ -0,0 +1,16 @@
+class mail {
+
+  package { "postfix":
+    ensure => present
+  }
+
+  package { "mailutils":
+    ensure => present
+  }
+
+  exec { 'autostartmail':
+    command => '/usr/sbin/update-rc.d postfix defaults',
+    require => Package['postfix']
+  }
+
+}
